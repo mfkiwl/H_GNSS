6b60e97fdb0d397ce030b67342162c0189bb6cb4
Sergey Maslennikov <maslennikovserge@yandex.ru>
2021-04-16 16:28:13 +0300

    Add: exit-codes

--- Gnss/main.cpp
+++ Gnss/main.cpp
@@ -12,6 +12,29 @@
 
 #include <boost/asio.hpp>
 
+//from /usr/include/sysexits.h
+enum class tExitCode : int
+{
+	EX_OK = 0,    // successful termination
+	EX__BASE = 64,    // base value for error messages
+	EX_USAGE = 64,    // command line usage error
+	EX_DATAERR = 65,    // data format error
+	EX_NOINPUT = 66,    // cannot open input
+	EX_NOUSER = 67,    // addressee unknown
+	EX_NOHOST = 68,    // host name unknown
+	EX_UNAVAILABLE = 69,    // service unavailable
+	EX_SOFTWARE = 70,    // internal software error
+	EX_OSERR = 71,    // system error (e.g., can't fork)
+	EX_OSFILE = 72,    // critical OS file missing
+	EX_CANTCREAT = 73,    // can't create (user) output file
+	EX_IOERR = 74,    // input/output error
+	EX_TEMPFAIL = 75,    // temp failure; user is invited to retry
+	EX_PROTOCOL = 76,    // remote error in protocol
+	EX_NOPERM = 77,    // permission denied
+	EX_CONFIG = 78,    // configuration error
+	EX__MAX = 78,    // maximum listed value
+};
+
 namespace dev
 {
 	void ThreadFunShell();
@@ -123,10 +146,10 @@ int main(int argc, char* argv[])
 	{
 		std::cerr << "Exception: " << e.what() << "\n";
 
-		return 1;//ERROR
+		return static_cast<int>(tExitCode::EX_CONFIG);
 	}
 
-	int CErr = 0;
+	int CErr = static_cast<int>(tExitCode::EX_OK);
 	////////////////////////////////
 	std::thread Thread_Shell;
 
@@ -142,7 +165,7 @@ int main(int argc, char* argv[])
 	try
 	{
 		if (Thread_GNSS_Future.get())
-			CErr = 2;
+			CErr = static_cast<int>(tExitCode::EX_NOINPUT);
 	}
 	catch (std::exception & e)
 	{
@@ -150,7 +173,7 @@ int main(int argc, char* argv[])
 
 		g_DataSetMainControl.Thread_GNSS_State = tDataSetMainControl::tStateGNSS::Exit;
 
-		CErr = 3;
+		CErr = static_cast<int>(tExitCode::EX_IOERR);
 	}
 
 	Thread_GNSS.join();
